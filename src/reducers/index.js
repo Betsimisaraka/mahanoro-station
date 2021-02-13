e6ccc9fd73b6b8f937f6a15db4acb61cabc91b70
Anita RASOA <anita.ras@onja.org>
2021-02-13 09:57:55 +0300

    Setting up reducers and the store

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import {combineReducers} from 'redux';
+
+function cities(state=[], action) {
+    return state
+}
+
+function users(state=[], action) {
+    return state
+}
+
+export default combineReducers({
+    cities,
+    users,
+})
---
+++ src/state.js
@@ -0,0 +1,11 @@
+export default {
+    cities: [],
+    users: [
+        {
+            id: 1,
+            firstName: "Anita",
+            lastname: "Rasoa",
+            phoneNumber: "0345291094"
+        }
+    ]
+}
